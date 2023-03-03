c80306dceadccec52dd7e511bde272fff95fb767
Avinash Magar <42104652+Avinash174@users.noreply.github.com>
2023-03-03 09:45:03 +0530

    New Changes Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:collage_recommandation/screens/Auth_Screen/log_in/Login.dart';
+import 'package:collage_recommandation/screens/Auth_Screen/log_in/log_in.dart';
 import 'package:flutter/material.dart';
 
 void main() {
---
+++ lib/screens/Auth_Screen/log_in/log_in.dart
@@ -0,0 +1,95 @@
+import 'package:collage_recommandation/screens/Auth_Screen/sign_up/sign_up.dart';
+import 'package:collage_recommandation/screens/homepage/home.dart';
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatefulWidget {
+  const LogInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LogInScreen> createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
+  TextEditingController userName = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Log In'),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios_new),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 200,
+              width: 250,
+              child: Icon(Icons.person),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'User Name',
+                    hintText: 'Enter valid mail id as abc@gmail.com'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter your secure password'),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                //TODO FORGOT PASSWORD SCREEN GOES HERE
+              },
+              child: Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => HomePage()));
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 130,
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => const SignUpScreen()));
+              },
+              child: const Text('New User? Create Account'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
